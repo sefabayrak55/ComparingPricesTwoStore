91ad3c7c984cd80b06cb75d0085b54bcb4b78e93
sefabayrak55 <sefabayrak55@hotmail.com>
2023-05-10 13:11:51 +0300

    10052023

--- src/test/java/comparingPrice/comparingPrice.java
+++ src/test/java/comparingPrice/comparingPrice.java
@@ -9,10 +9,12 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class comparingPrice {
     static WebDriver driver;
@@ -20,7 +22,11 @@ public class comparingPrice {
     @BeforeClass
     public static void setup(){
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
